7356789358bb8d4cedaf7aba9ae42b611de3d83f
Denis <60257924+denrybaakov@users.noreply.github.com>
2022-02-15 20:24:28 +0300

    Delete build directory

--- build/MatchResult.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MatchResult;
-(function (MatchResult) {
-    MatchResult["HomeWin"] = "H";
-    MatchResult["AwayWin"] = "A";
-    MatchResult["Draw"] = "D";
-})(MatchResult || (MatchResult = {}));
-exports.default = MatchResult;
