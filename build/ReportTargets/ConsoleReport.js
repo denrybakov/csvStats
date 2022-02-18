1089b13e20c10f252ed86d493bb7d77d776c0be4
Denis <60257924+denrybaakov@users.noreply.github.com>
2022-02-18 18:46:26 +0300

    Delete build directory

--- build/ReportTargets/ConsoleReport.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ConsoleReport = /** @class */ (function () {
-    function ConsoleReport() {
-        this.print = function (report) { return console.log(report); };
-    }
-    return ConsoleReport;
-}());
-exports.default = ConsoleReport;
